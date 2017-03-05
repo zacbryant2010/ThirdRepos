73d7b9ce99c6a3fb2565f76c2daf4d0552438685
Zac <zac.bryant15@gmail.com>
2017-03-05 18:16:06 +1030

    commit the child discount changes

---
+++ ThirdRepos/MyInitial/TicketsForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ticketing
+{
+    public partial class TicketsForm : Form
+    {
+        TicketPrice mTicketPrice;
+        int mSection = 2;
+        int mQuantity = 0;
+        int mDiscount = 0;
+
+        public TicketsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TicketsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCalculate_Click(object sender, EventArgs e)
+        {
+            mQuantity = int.Parse(txtQuantity.Text);
+
+            if (chkDiscount.Checked)
+                { mDiscount = 1; }
+            else if (chkChild.Checked)
+                { mDiscount = 2; }
+            else if (!chkDiscount.Checked && !chkChild.Checked)
+                    { mDiscount = 0; }
+
+            if (radBalcony.Checked)
+                { mSection = 1; }
+            if (radGeneral.Checked)
+                { mSection = 2; }
+            if (radBox.Checked)
+                { mSection = 3; }
+
+            mTicketPrice = new TicketPrice(mSection, mQuantity, mDiscount);
+
+            mTicketPrice.calculatePrice();
+            lblAmount.Text = System.Convert.ToString(mTicketPrice.AmountDue);
+        }
+
+        private void chkDiscount_CheckedChanged(object sender, EventArgs e)
+        {
+            chkChild.Checked = false;
+        }
+
+        private void chkChild_CheckedChanged(object sender, EventArgs e)
+        {
+            chkDiscount.Checked = false;
+        }
+     }
+}
